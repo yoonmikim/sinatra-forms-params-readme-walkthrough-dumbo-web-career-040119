b9a55538761abe086854c2d485de83ed03385388
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-22 21:49:41 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   # Add your post route and action below
-  get '/food_form'
-  erb :food_form
+  post '/food' do
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
   end
 end
